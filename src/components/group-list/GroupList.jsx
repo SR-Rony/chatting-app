9b98ab1d35e0598340dd5bdde9f49c5b052f11ee
SR-Rony <srrony707@gmail.com>
2023-10-15 11:39:45 -0700

    group is add

--- src/components/group-list/GroupList.jsx
+++ src/components/group-list/GroupList.jsx
@@ -1,53 +1,119 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hadding from '../hadding/Hadding'
 import Images from '../images/Images'
 import img from '../../assets/img.png'
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Paragraph from '../paragraph/Paragraph';
+import TextField from '@mui/material/TextField';
+import { getDatabase, push, ref, set,onValue} from "firebase/database";
+import { useSelector } from 'react-redux';
+
+
+// modal style
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const GroupList = () => {
+    const db = getDatabase();
+    const [group,setGroup]=useState({gname:'',gtitle:''})
+    const [groupArray,setGroupArray]=useState([])
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const userInfo =useSelector(state=>state.loginSlice.value)
+
+    useEffect(()=>{
+        const starCountRef = ref(db, 'group');
+        onValue(starCountRef, (snapshot) => {
+            let array=[]
+            snapshot.forEach((item)=>{
+                if(userInfo.uid!=item.val().groupAdminId){
+                    array.push({...item.val(),'groupId':item.key})
+                }
+            })
+            setGroupArray(array)
+        });
+    },[])
+
+
+
+
+
+
+    // group input change
+    const handleChang =(e)=>{
+        setGroup({...group,[e.target.name]:e.target.value})
+    }
+
+    // add new group button
+    const handleGroupAdd =()=>{
+        set(push(ref(db, 'group')),{
+            groupName: group.gname,
+            groupTitle: group.gtitle,
+            groupAdmin:userInfo.displayName,
+            groupAdminId:userInfo.uid
+          });
+    }
+
+
+    // user group join
+    const handleGroupJoin =(group)=>{
+        set(push(ref(db, 'groupRequest')),{
+            ...group,
+            groupRequestName:userInfo.displayName,
+            groupRequestId:userInfo.uid
+          });
+
+    }
+
   return (
     <div className='box'>
-        <Hadding text ='Groups List'/>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
+        <div className="flex">
+            <Hadding text ='Groups List'/>
+            <Button onClick={handleOpen} variant="contained">create group</Button>
         </div>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
-        </div>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
-        </div>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
-        </div>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
-        </div>
+        {groupArray.map((group)=>(
+             <div className='list'>
+             <Images className='list-img' src={img} />
+             <div className="text">
+                 <Hadding text ={group.groupName}/>
+             </div>
+             <Button onClick={()=>handleGroupJoin(group)} variant="contained">join</Button>
+         </div>
+        ))}
+        {/* create group modal */}
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Create New Group
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <Paragraph  text='Group Name :'/>
+            <TextField onChange={handleChang} name='gname'id="outlined-basic" label="inter your group name" variant="outlined" />
+            <Paragraph text='Group Title :'/>
+            <TextField onChange={handleChang} name='gtitle' id="outlined-basic" label="inter your group title" variant="outlined" />
+          </Typography>
+          <Button onClick={handleGroupAdd} variant="contained">Add Group</Button>
+        </Box>
+      </Modal>
     </div>
   )
 }
--- src/components/my-groups/MyGroups.jsx
+++ src/components/my-groups/MyGroups.jsx
@@ -1,53 +1,127 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hadding from '../hadding/Hadding'
 import Images from '../images/Images'
 import img from '../../assets/img.png'
 import Button from '@mui/material/Button';
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useSelector } from 'react-redux';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const MyGroups = () => {
+    const[groupArray,setGroupArray]=useState([])
+    const[groupRequest,setGeoupRequest]=useState([])
+    const db = getDatabase();
+    const userInfo =useSelector(state=>state.loginSlice.value)
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () =>{
+        setOpen(true)
+        const groupRequestRef = ref(db, 'groupRequest' );
+        onValue(groupRequestRef, (snapshot) => {
+            let array=[]
+            snapshot.forEach((item)=>{
+                    array.push(item.val())
+            })
+            setGeoupRequest(array)
+        });
+    }
+    const handleClose = () => setOpen(false);
+
+    useEffect(()=>{
+        const groupRef = ref(db, 'group' );
+        onValue(groupRef, (snapshot) => {
+            let array=[]
+            snapshot.forEach((item)=>{
+                if(item.val().groupAdminId==userInfo.uid){
+                    array.push(item.val())
+                }
+            })
+            setGroupArray(array)
+        });
+    },[])
+
+    // group member button
+    const handleGroupMember =()=>{
+        console.log('member');
+    }
+
   return (
     <div className='box'>
         <Hadding text ='My Groups'/>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
+
+        {groupArray.map((group)=>(
+            <div className='list'>
+                <Images className='list-img' src={img} />
+                <div className="text">
+                    <Hadding text ={group.groupName}/>
+                </div>
+                <Button onClick={handleGroupMember} variant="contained">Ml</Button>
+                <Button onClick={handleOpen}  variant="contained">Rl</Button>
+                <Button color='error'  variant="contained">d</Button>
             </div>
-            <Button variant="contained">join</Button>
-        </div>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
-        </div>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
-        </div>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
-        </div>
-        <div className='list'>
-            <Images className='list-img' src={img} />
-            <div className="text">
-                <Hadding text ='Friends Reunion'/>
-                <p>Hi Guys, Wassup!</p>
-            </div>
-            <Button variant="contained">join</Button>
-        </div>
+        ))}
+
+
+
+        {/* group request list  */}
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+                people wants to join my group
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                {groupRequest.map((item)=>(
+                    <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                    primary={item.groupRequestName}
+                    secondary={
+                        <React.Fragment>
+                        <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                        >
+                            Ali Connors
+                        </Typography>
+                        {" — I'll be in your neighborhood doing errands this…"}
+                        </React.Fragment>
+                    }
+                    />
+                </ListItem>
+                ))}
+            </List>
+            </Typography>
+            </Box>
+      </Modal>
     </div>
   )
 }
